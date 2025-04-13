7cf944590271b9c748209d1735270f81056173cd
bigoldcat123 <a1456870251@163.com>
2025-04-13 22:25:20 +0800

    init

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,12 +6,13 @@ import { boolean, date, integer, pgTable, text, varchar } from "drizzle-orm/pg-c
 
 export const classMark = pgTable("classMark", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    finishDate: date().notNull(),
+    finishDate: date({mode:'date'}).notNull(),
     class_and_room: text().notNull(),
     name: text().notNull(),
     order: integer().notNull(),
     week: integer().notNull(),      // 1~7，7 表示自习
 });
+type x = typeof classMark.$inferInsert;
 export const classTable = pgTable("classTable", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: text().notNull(),
@@ -27,5 +28,5 @@ export const tempClassTable = pgTable("tempClassTable", {
     week: integer().notNull(),      // 1~7，7 表示自习
     order: integer().notNull(),     // 1~7
     type: boolean().notNull(),      // true 为新增，false 为删除
-    execDate: date().notNull(),     // 执行时间
+    execDate: date({mode:'date'}).notNull(),     // 执行时间
 });
