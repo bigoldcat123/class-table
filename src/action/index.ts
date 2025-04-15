2de5437af10e6c848b7e1fbbe8035869f9b8c866
bigoldcat123 <a1456870251@163.com>
2025-04-15 20:20:11 +0800

    switch

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,141 +1,258 @@
-'use server'
-import { z } from 'zod'
-import { TableCellProp } from "@/components/tableCell"
-import db from "@/db"
-import { classMark, classTable, tempClassTable } from "@/db/schema"
-import { and, desc, eq, gte, like, lte } from "drizzle-orm"
-import { revalidatePath } from 'next/cache'
+"use server";
+import { z } from "zod";
+import { Coordinate, TableCellProp } from "@/components/tableCell";
+import db from "@/db";
+import { classMark, classTable, tempClassTable } from "@/db/schema";
+import { and, desc, eq, gte, like, lte } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export async function getClassTable(date: Date) {
+  const m_s = getWeekAround(date);
 
-    const m_s = getWeekAround(date)
+  const classes = await db.select().from(classTable);
 
-    const classes = await db.select().from(classTable)
+  const tempClasses = await db
+    .select()
+    .from(tempClassTable)
+    .where(
+      and(
+        gte(tempClassTable.execDate, m_s.monday),
+        lte(tempClassTable.execDate, m_s.sunday),
+      ),
+    );
 
-    const tempClasses = await db.select().from(tempClassTable).where(and(gte(tempClassTable.execDate, m_s.monday), lte(tempClassTable.execDate, m_s.sunday)))
+  const classMarks = await db
+    .select()
+    .from(classMark)
+    .where(
+      and(
+        gte(classMark.finishDate, m_s.monday),
+        lte(classMark.finishDate, m_s.sunday),
+      ),
+    );
+  const map = new Map<string, TableCellProp>();
+  classes.forEach((x) => {
+    map.set(`${x.week}${x.order}`, {
+      classAndRoom: x.class_and_room,
+      name: x.name,
+      isTemp: false,
+      isFinished: false,
+    });
+  });
+  // console.log(tempClasses);
 
-    const classMarks = await db.select().from(classMark).where(and(gte(classMark.finishDate, m_s.monday), lte(classMark.finishDate, m_s.sunday)))
-    const map = new Map<string, TableCellProp>()
-    classes.forEach(x => {
-        map.set(`${x.week}${x.order}`, { classAndRoom: x.class_and_room, name: x.name, isTemp: false, isFinished: false })
-    })
-    // console.log(tempClasses);
+  tempClasses.forEach((x) => {
+    if (x.type) {
+      // add
+      map.set(`${x.week}${x.order}`, {
+        classAndRoom: x.class_and_room,
+        name: x.name,
+        isTemp: true,
+        isFinished: false,
+      });
+    } else {
+      map.delete(`${x.week}${x.order}`);
+    }
+  });
+  classMarks.forEach((x) => {
+    const classData = map.get(`${x.week}${x.order}`)!;
+    classData.isFinished = true;
+    map.set(`${x.week}${x.order}`, classData as TableCellProp);
+  });
 
-    tempClasses.forEach(x => {
-        if (x.type) {// add
-            map.set(`${x.week}${x.order}`, { classAndRoom: x.class_and_room, name: x.name, isTemp: true, isFinished: false })
-        } else {
-            map.delete(`${x.week}${x.order}`)
-        }
-    })
-    classMarks.forEach(x => {
-        const classData = map.get(`${x.week}${x.order}`)!
-        classData.isFinished = true;
-        map.set(`${x.week}${x.order}`, classData as TableCellProp)
-    })
-
-    return map
+  return map;
 }
-function getWeekAround(date: Date): { monday: Date, sunday: Date } {
-    const day = date.getDay(); // 0 (Sunday) to 6 (Saturday)
-    const diffToMonday = day === 0 ? -6 : 1 - day;
-    const diffToSunday = day === 0 ? 0 : 7 - day;
+function getWeekAround(date: Date): { monday: Date; sunday: Date } {
+  const day = date.getDay(); // 0 (Sunday) to 6 (Saturday)
+  const diffToMonday = day === 0 ? -6 : 1 - day;
+  const diffToSunday = day === 0 ? 0 : 7 - day;
 
-    const monday = new Date(date);
-    monday.setDate(date.getDate() + diffToMonday);
+  const monday = new Date(date);
+  monday.setDate(date.getDate() + diffToMonday);
 
-    const sunday = new Date(date);
-    sunday.setDate(date.getDate() + diffToSunday);
+  const sunday = new Date(date);
+  sunday.setDate(date.getDate() + diffToSunday);
 
-    // 清除时间部分，仅保留日期（可选）
-    monday.setHours(0, 0, 0, 0);
-    sunday.setHours(23, 59, 59, 999);
+  // 清除时间部分，仅保留日期（可选）
+  monday.setHours(0, 0, 0, 0);
+  sunday.setHours(23, 59, 59, 999);
 
-    return { monday, sunday };
+  return { monday, sunday };
 }
 
 const tempClassSchema = z.object({
-    type: z.coerce.boolean(),
-    name: z.string(),
-    class_and_room: z.string(),
-    week: z.number().optional(),
-    order: z.coerce.number(),
-    execDate: z.coerce.date(),
-})
+  type: z.coerce.boolean(),
+  name: z.string(),
+  class_and_room: z.string(),
+  week: z.number().optional(),
+  order: z.coerce.number(),
+  execDate: z.coerce.date(),
+});
 
 export async function addTempAction(stat: string, formData: FormData) {
-    const obj = Object.fromEntries(formData)
-    
+  const obj = Object.fromEntries(formData);
 
-    const temp = tempClassSchema.parse(obj)
-    temp.order -= 1
-    const day = temp.execDate.getDay() == 0 ? 6 : temp.execDate.getDay() - 1
-    console.log(day);
+  const temp = tempClassSchema.parse(obj);
+  temp.order -= 1;
+  const day = temp.execDate.getDay() == 0 ? 6 : temp.execDate.getDay() - 1;
+  console.log(day);
 
-    temp.week = day
-    const order = temp.order
-    const table = await getClassTable(temp.execDate)
-    if (table.has(`${day}${order}`) && temp.type) {
-        return "conflicten!" + JSON.stringify(table.get(`${day}${order}`))
-    }
-    await db.insert(tempClassTable).values(temp as typeof tempClassTable.$inferSelect)
-    revalidatePath("/", 'page')
-    return stat + "ok!"
+  temp.week = day;
+  const order = temp.order;
+  const table = await getClassTable(temp.execDate);
+  if (table.has(`${day}${order}`) && temp.type) {
+    return "conflicten!" + JSON.stringify(table.get(`${day}${order}`));
+  }
+  await db
+    .insert(tempClassTable)
+    .values(temp as typeof tempClassTable.$inferSelect);
+  revalidatePath("/", "page");
+  return stat + "ok!";
 }
 
 export async function getTempAction() {
-    const temps = await db.select().from(tempClassTable).orderBy(desc(tempClassTable.execDate))
-    return temps
+  const temps = await db
+    .select()
+    .from(tempClassTable)
+    .orderBy(desc(tempClassTable.execDate));
+  return temps;
 }
 export async function deleteTempAction(id: number) {
-    const res = await db.delete(tempClassTable).where(eq(tempClassTable.id, id))
-    revalidatePath('/temp')
+  const res = await db.delete(tempClassTable).where(eq(tempClassTable.id, id));
+  revalidatePath("/temp");
 }
 
-export async function markAsFinishedAction({ clazz, coordinate }: {
-    coordinate: {
-        week: number,
-        order: number
-    },
-    clazz: TableCellProp
+export async function markAsFinishedAction({
+  clazz,
+  coordinate,
+}: {
+  coordinate: {
+    week: number;
+    order: number;
+  };
+  clazz: TableCellProp;
 }) {
-    if (clazz.isFinished) {
-        return
-    }
-    const mark: typeof classMark.$inferInsert = {
-        class_and_room: clazz.classAndRoom,
-        finishDate: new Date(),
-        name: clazz.name,
-        order: coordinate.order,
-        week: coordinate.week,
-    }
-    await db.insert(classMark).values(mark)
-    revalidatePath('/')
+  if (clazz.isFinished) {
+    return;
+  }
+  const mark: typeof classMark.$inferInsert = {
+    class_and_room: clazz.classAndRoom,
+    finishDate: new Date(),
+    name: clazz.name,
+    order: coordinate.order,
+    week: coordinate.week,
+  };
+  await db.insert(classMark).values(mark);
+  revalidatePath("/");
 }
 const getJobMarkActionQuery = z.object({
-    name: z.string().optional(),
-    startDate: z.coerce.date().optional(),
-    endDate: z.coerce.date().optional()
-})
-export type GetJobMarkActionQuery = z.infer<typeof getJobMarkActionQuery>
+  name: z.string().optional(),
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
+});
+export type GetJobMarkActionQuery = z.infer<typeof getJobMarkActionQuery>;
 
-export async function getJobMarkAction(query: { name?: string, startDate?: string, endDate?: string } = { name: undefined, startDate: undefined, endDate: undefined }) {
+export async function getJobMarkAction(
+  query: { name?: string; startDate?: string; endDate?: string } = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+  },
+) {
+  const queryParsed = getJobMarkActionQuery.parse(query);
 
-    const queryParsed = getJobMarkActionQuery.parse(query)
+  if (queryParsed.endDate) {
+    queryParsed.endDate.setHours(23, 59, 59, 999);
+  }
+  if (queryParsed.startDate) {
+    queryParsed.startDate.setHours(0, 0, 0, 0);
+  }
 
-    if (queryParsed.endDate) {
-        queryParsed.endDate.setHours(23, 59, 59, 999)
-    }
-    if (queryParsed.startDate) {
-        queryParsed.startDate.setHours(0, 0, 0, 0)
-    }
-
-    const marks = await db.select().from(classMark).orderBy(desc(classMark.finishDate)).where(
-        and(
-            queryParsed.name ? like(classMark.name, `%${queryParsed.name}%`) : undefined,
-            queryParsed.startDate ? gte(classMark.finishDate, queryParsed.startDate) : undefined,
-            queryParsed.endDate ? lte(classMark.finishDate, queryParsed.endDate) : undefined
-        )
+  const marks = await db
+    .select()
+    .from(classMark)
+    .orderBy(desc(classMark.finishDate))
+    .where(
+      and(
+        queryParsed.name
+          ? like(classMark.name, `%${queryParsed.name}%`)
+          : undefined,
+        queryParsed.startDate
+          ? gte(classMark.finishDate, queryParsed.startDate)
+          : undefined,
+        queryParsed.endDate
+          ? lte(classMark.finishDate, queryParsed.endDate)
+          : undefined,
+      ),
     );
-    return marks;
+  return marks;
+}
+
+export type TableCellPropActionFrom = {
+  tableCell: TableCellProp;
+  coordinate: Coordinate;
+};
+export type TableCellPropActionTo = {
+  tableCell?: TableCellProp;
+  coordinate: Coordinate;
+};
+export async function swicthAction(
+  from: TableCellPropActionFrom,
+  to: TableCellPropActionTo,
+) {
+  const { monday } = getWeekAround(new Date());
+  const dayToDelFrom = new Date(monday);
+  const dayToAddFrom = new Date(monday);
+
+  dayToDelFrom.setDate(monday.getDate() + from.coordinate.week);
+  dayToAddFrom.setDate(monday.getDate() + to.coordinate.week);
+  const tempDelFrom: typeof tempClassTable.$inferInsert = {
+    execDate: dayToDelFrom,
+    name: "change",
+    class_and_room: "change",
+    week: from.coordinate.week,
+    order: from.coordinate.order,
+    type: false,
+  };
+  const tempAddFrom: typeof tempClassTable.$inferInsert = {
+    execDate: dayToAddFrom,
+    name: from.tableCell.name,
+    class_and_room: from.tableCell.classAndRoom,
+    week: to.coordinate.week,
+    order: to.coordinate.order,
+    type: true,
+  };
+  if (to.tableCell) {
+    // switch
+    const dayToDelTo = new Date(monday);
+    const dayToAddTo = new Date(monday);
+
+    dayToDelTo.setDate(monday.getDate() + to.coordinate.week);
+    dayToAddTo.setDate(monday.getDate() + from.coordinate.week);
+
+    const tempDelTo: typeof tempClassTable.$inferInsert = {
+      execDate: dayToDelTo,
+      name: "switch",
+      class_and_room: "switch",
+      week: to.coordinate.week,
+      order: to.coordinate.order,
+      type: false,
+    };
+    const tempAddTo: typeof tempClassTable.$inferInsert = {
+      execDate: dayToAddTo,
+      name: to.tableCell.name,
+      class_and_room: to.tableCell.classAndRoom,
+      week: from.coordinate.week,
+      order: from.coordinate.order,
+      type: true,
+    };
+    await db
+      .insert(tempClassTable)
+      .values([tempDelFrom, tempDelTo, tempAddFrom, tempAddTo]);
+  } else {
+    // just change
+
+    await db.insert(tempClassTable).values([tempDelFrom, tempAddFrom]);
+  }
+  revalidatePath("/");
 }
--- src/components/tableCell.tsx
+++ src/components/tableCell.tsx
@@ -1,60 +1,81 @@
-'use client'
-import { markAsFinishedAction } from "@/action";
+"use client";
+import {
+  markAsFinishedAction,
+  swicthAction,
+  TableCellPropActionFrom,
+  TableCellPropActionTo,
+} from "@/action";
 import clsx from "clsx";
 
 export type TableCellProp = {
-    name: string,
-    classAndRoom: string,
-    isTemp: boolean,
-    isFinished: boolean
-}
+  name: string;
+  classAndRoom: string;
+  isTemp: boolean;
+  isFinished: boolean;
+};
+export type Coordinate = {
+  week: number;
+  order: number;
+};
 type Props = {
-    clx: TableCellProp | undefined,
-    className?: string,
-    coordinate: {
-        week: number,
-        order: number
-    }
-}
+  clx: TableCellProp | undefined;
+  className?: string;
+  coordinate: Coordinate;
+};
 export default function TableCell({ clx, className, coordinate }: Props) {
-    const markAsFinished = async () => {
-        if (!clx || clx.isFinished) {
-            return
-        }
-        if (confirm("完成？")) {
-            await markAsFinishedAction({ clazz: clx!, coordinate })
-        }
+  const markAsFinished = async () => {
+    if (!clx || clx.isFinished) {
+      return;
     }
+    if (confirm("完成？")) {
+      await markAsFinishedAction({ clazz: clx!, coordinate });
+    }
+  };
 
-    return (
-        <>
-            <div draggable={clx && !clx.isFinished && !clx.isTemp}
-                onDragStart={(e) => {
-                    e.dataTransfer.setData("coordinate", JSON.stringify({ ...coordinate, ...clx }))
-                }}
-                onDragOver={(e) => e.preventDefault()}
-                onDrop={(e) => {
-                    e.preventDefault()
-                    if(clx?.isFinished || clx?.isTemp) {
-                        alert("不可以这么换哦！")
-                        return
-                    }
-                    const from = JSON.parse(e.dataTransfer.getData("coordinate"))
-                    console.log("move " + JSON.stringify(from) + " to " + JSON.stringify(coordinate));
-                    if (clx) {
-                        console.log("switch");
-                    } else {
-                        console.log("change");
-                    }
-                }}
-                onClick={() => markAsFinished()}
-                className={clsx(" cursor-pointer min-w-[9vw] max-w-[9vw] px-2 h-[5vw] border-2 flex flex-col items-center justify-center", className, {
-                    'border-amber-500': clx?.isTemp,
-                    'bg-green-400': clx?.isFinished,
-                })}>
-                <h1 className=" text-[1.5vw]">{clx?.name}</h1>
-                <p className=" text-[1vw]">{clx?.classAndRoom}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div
+        draggable={clx && !clx.isFinished && !clx.isTemp}
+        onDragStart={(e) => {
+          e.dataTransfer.setData(
+            "coordinate",
+            JSON.stringify({ coordinate, tableCell: clx }),
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e) => {
+          e.preventDefault();
+          if (clx?.isFinished || clx?.isTemp) {
+            alert("不可以这么换哦！");
+            return;
+          }
+          const from = JSON.parse(
+            e.dataTransfer.getData("coordinate"),
+          ) as TableCellPropActionFrom;
+          const to: TableCellPropActionTo = {
+            coordinate,
+            tableCell: clx,
+          };
+          swicthAction(from, to);
+          if (clx) {
+            console.log("switch");
+          } else {
+            console.log("change");
+          }
+        }}
+        onClick={() => markAsFinished()}
+        className={clsx(
+          " cursor-pointer min-w-[9vw] max-w-[9vw] px-2 h-[5vw] border-2 flex flex-col items-center justify-center",
+          className,
+          {
+            "border-amber-500": clx?.isTemp,
+            "bg-green-400": clx?.isFinished,
+          },
+        )}
+      >
+        <h1 className=" text-[1.5vw]">{clx?.name}</h1>
+        <p className=" text-[1vw]">{clx?.classAndRoom}</p>
+      </div>
+    </>
+  );
 }
